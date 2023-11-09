7ae2d365c15a9f9e682074a17e6b59a62b139952
Clopez <cluis.lopez@gmail.com>
2023-11-09 13:06:06 +0100

    Firs commit

---
+++ Bricks/main.js
@@ -0,0 +1,417 @@
+const SCALE = 5
+const BOARD_WIDTH = 150 // 750 pixels
+const BOARD_HEIGHT = 100 //500 pixels
+const PADDLE_WIDTH = 16
+const PADDLE_PADDING = 2
+const PADDLE_HEIGHT = 1
+const PADDLE_SPEED = 3
+const BALL_SIZE = 1 // 5 scaled pixels
+
+const BLOCK_TOP = 10
+const BLOCK_GAP = 10
+
+const NUM_BALLS_LEVEL = 5
+
+const SCREENS = [
+    [ //Level 1
+        {
+            color: 'red',
+            blocks_in_line: 3,
+            block_heigh: 2,
+            block_width: 15,
+            gapNext: 6
+        },
+
+        {
+            color: 'yellow',
+            blocks_in_line: 6,
+            block_heigh: 2,
+            block_width: 15,
+            gapNext: 4
+        },
+
+        {
+            color: 'magenta',
+            blocks_in_line: 6,
+            block_heigh: 1,
+            block_width: 15,
+            gapNext: 4
+        },
+
+        {
+            color: 'blue',
+            blocks_in_line: 6,
+            block_heigh: 2,
+            block_width: 10,
+            gapNext: 4
+        }
+    ],
+    [ //Level 2
+        {
+            color: 'pink',
+            blocks_in_line: 8,
+            block_heigh: 3,
+            block_width: 5,
+            gapNext: 4
+        },
+
+        {
+            color: 'lightblue',
+            blocks_in_line: 8,
+            block_heigh: 3,
+            block_width: 5,
+            gapNext: 4
+        },
+
+        {
+            color: 'brown',
+            blocks_in_line: 8,
+            block_heigh: 3,
+            block_width: 5,
+            gapNext: 4
+        },
+
+        {
+            color: 'salmon',
+            blocks_in_line: 8,
+            block_heigh: 3,
+            block_width: 5,
+            gapNext: 4
+        },
+
+        {
+            color: 'red',
+            blocks_in_line: 10,
+            block_heigh: 4,
+            block_width: 3,
+            gapNext: 4
+        }
+    ]
+]
+
+const PADDLE_HPOS = BOARD_HEIGHT - PADDLE_PADDING - PADDLE_HEIGHT
+const canvas = document.getElementById("app")
+const context = canvas.getContext("2d")
+const ballsToLive = document.getElementById("balls")
+const levelToPlay = document.getElementById("level")
+
+canvas.width = BOARD_WIDTH * SCALE
+canvas.height = BOARD_HEIGHT * SCALE
+context.scale(SCALE, SCALE)
+rebootBall = new Audio("./rebootClick.wav")
+removedBlock = new Audio("./removedBlock.wav")
+lostBall = new Audio("./lostBall.wav")
+gameOver = new Audio("./gameOver.wav")
+
+
+class Cell {
+    value
+    color
+    constructor(val, col) {
+        this.value = val
+        this.color = col
+    }
+}
+
+class Paddle {
+    posX
+    previousPosX
+    width
+    constructor(level) {
+        this.width = PADDLE_WIDTH - level
+        this.posX = BOARD_WIDTH / 2 - this.width / 2
+        this.previousPosX = this.posX
+    }
+
+    moveLeft() {
+        this.previousPosX = this.posX
+        if (this.posX > 0) this.posX -= PADDLE_SPEED
+    }
+
+    moveRight() {
+        this.previousPosX = this.posX
+        if (this.posX < BOARD_WIDTH - this.width) this.posX += PADDLE_SPEED
+    }
+
+    updatePaddle() {
+        //Borra la antigua paleta
+        context.fillStyle = 'black'
+        context.fillRect(this.previousPosX, PADDLE_HPOS, this.width, PADDLE_HEIGHT)
+        //Dibuja la paleta
+        context.fillStyle = 'lightsalmon'
+        context.fillRect(this.posX, PADDLE_HPOS, this.width, PADDLE_HEIGHT)
+    }
+}
+
+class Ball {
+    position = {}
+    previousP = {}
+    direction = {}
+    constructor() {
+        this.position.x = Math.floor(Math.random() * (BOARD_WIDTH - BALL_SIZE))
+        this.position.y = BOARD_HEIGHT - PADDLE_PADDING - PADDLE_HEIGHT
+        this.direction.x = (Math.floor(Math.random()) == 0 ? -1 : 1) * BALL_SIZE
+        this.direction.y = - BALL_SIZE //Hacia arriba
+    }
+
+    move(p, b) {
+        this.previousP.x = this.position.x
+        this.previousP.y = this.position.y
+        this.position.x += this.direction.x
+        this.position.y += this.direction.y
+        return this.checkCollision(p, b)
+    }
+
+    checkCollision(p, b) {
+        if (this.position.y <= 0) {
+            this.position.y = 0
+            this.direction.y = BALL_SIZE
+            return true
+        }
+        if (this.position.x <= 0 || this.position.x >= BOARD_WIDTH) {
+            this.position.x -= this.direction.x
+            this.direction.x *= -1
+            return true
+        }
+        if (b.board[this.position.y][this.position.x].value != 0) {
+            b.removeBlock(this.position.y, this.position.x)
+            b.initDrawBoard()
+            this.direction.y *= -1
+            return true
+        }
+        if (this.position.y == PADDLE_HPOS)
+            if (this.position.x >= p.posX && this.position.x <= p.posX + p.width) {
+                this.direction.y = -BALL_SIZE
+                if (this.position.x <= p.posX + p.width / 3 || this.position.x >= p.posX + 2 * p.width / 3)
+                    this.direction.x *= -1
+                rebootBall.play();
+                return true
+            } else {
+                return false
+            }
+        return true
+    }
+
+    removePrevious() {
+        //Borra la antigua posicion
+        context.fillStyle = 'black'
+        context.fillRect(this.previousP.x, this.previousP.y, BALL_SIZE, BALL_SIZE)
+    }
+
+    updateBall() {
+        this.removePrevious()
+        //Dibuja la nueva posición de la  pelotita
+        context.fillStyle = 'white'
+        context.fillRect(this.position.x, this.position.y, BALL_SIZE, BALL_SIZE)
+    }
+
+}
+
+class Board {
+
+    board = []
+    numBlocks = 0
+
+    constructor(level) {
+        let numRows = 0
+        for (let i = 0; i < BLOCK_TOP; i++) {
+            let row = []
+            for (let j = 0; j < BOARD_WIDTH; j++) {
+                row[j] = new Cell(0, 'black')
+            }
+            numRows++
+            this.board.push(row)
+        }
+
+        SCREENS[level - 1].forEach((i) => {
+            let row = []
+            let gap = Math.floor((BOARD_WIDTH - (i.block_width * i.blocks_in_line)) / (i.blocks_in_line + 1))
+            for (let k = 0; k < i.block_heigh; k++) {
+
+                //Fill of blank cells
+                for (let j = 0; j < BOARD_WIDTH; j++) {
+                    row[j] = new Cell(0, 'black')
+                }
+
+                let idx = gap
+
+                for (let l = 0; l < i.blocks_in_line; l++) {
+                    //Block
+                    this.numBlocks += 1
+                    for (let j = 0; (j < i.block_width) && (idx < BOARD_WIDTH); j++) {
+                        row[idx] = new Cell(1, i.color)
+                        idx++
+                    }//Gap: jump "gap" positions
+                    for (let j = 0; (j < gap) && (idx < BOARD_WIDTH); j++) {
+                        idx++
+                    }
+                }
+
+                numRows++
+                this.board.push(row)
+            }
+            //Gap vertical entre bloques
+            for (let k = 0; k < i.gapNext; k++) {
+                let row = []
+                for (let j = 0; j < BOARD_WIDTH; j++) {
+                    row[j] = new Cell(0, 'black')
+                }
+                numRows++
+                this.board.push(row)
+            }
+        })
+
+        for (let i = numRows; i < BOARD_HEIGHT; i++) {
+            let row = []
+            for (let j = 0; j < BOARD_WIDTH; j++) {
+                row[j] = new Cell(0, 'black')
+            }
+            numRows++
+            this.board.push(row)
+        }
+    }
+
+    removeBlock(r, c) {
+        this.numBlocks -= 1
+        removedBlock.play()
+        let rr = r
+        //Current Line
+        this.removeRow(r, c)
+        //Lines above
+        while (this.board[rr - 1][c].value != 0) {
+            this.removeRow(rr - 1, c)
+            --rr
+        }
+        rr = r
+        //Remove lines below
+        while (this.board[rr + 1][c].value != 0) {
+            this.removeRow(rr, c)
+            ++rr
+        }
+    }
+
+    removeRow(row, cell) {
+        let x = cell
+        while (this.board[row][x].value != 0) {
+            this.board[row][x].value = 0
+            this.board[row][x].color = 'black'
+            x++
+        }
+        x = cell - 1
+        while (this.board[row][x].value != 0) {
+            this.board[row][x].value = 0
+            this.board[row][x].color = 'black'
+            x--
+        }
+    }
+
+    initDrawBoard() {
+        for (let y = 0; y < BOARD_HEIGHT; y++) {
+            for (let x = 0; x < BOARD_WIDTH; x++) {
+                context.fillStyle = this.board[y][x].color
+                context.fillRect(x, y, 1, 1)
+            }
+        }
+    }
+
+}
+
+class Game {
+
+    numBalls
+    level
+    paddle
+    ball
+    board
+
+    constructor() {
+        this.level = 1
+        this.numBalls = NUM_BALLS_LEVEL
+        ballsToLive.innerText = "Vidas : " + this.numBalls
+        levelToPlay.innerText = "Nivel : " + this.level
+        this.paddle = new Paddle(this.level)
+        this.ball = new Ball()
+        this.board = new Board(this.level)
+    }
+
+    play() {
+
+        this.board.initDrawBoard()
+
+        document.addEventListener('keydown', event => {
+            if (event.key === 'ArrowLeft') {
+                this.paddle.moveLeft()
+            }
+            if (event.key === 'ArrowRight') {
+                this.paddle.moveRight()
+            }
+        })
+
+        playLevel(this.board, this.paddle, this.ball, (ret) => {
+
+            if (ret == "newBall") {
+                this.numBalls--
+                if (this.numBalls >= 0) {
+                    ballsToLive.innerText = "Vidas : " + this.numBalls
+                    this.ball = new Ball()
+                    this.play()
+                } else {
+                    gameOver.play()
+                    gameOver.onended = function () {window.alert("Game Over ... No more balls")}
+                }
+
+            } else if (ret == "newLevel") {
+                this.level++
+                if (this.level > SCREENS.length)
+                    window.alert ("End of game. No more levels to play")
+                else {
+                    this.board = new Board(this.level)
+                    this.paddle = new Paddle(this.level)
+                    this.ball = new Ball()
+                    this.play()
+                }
+            }
+        })
+    }
+}
+
+function playLevel(board, paddle, ball, callback) {
+
+    let dropCounter = 0
+    let lastTime = 0
+    let speed = 20
+    let score = 0
+    
+    update()
+
+    async function update(time = 0) {
+        const deltaTime = time - lastTime
+        let ret
+        lastTime = time
+
+        dropCounter += deltaTime
+
+        if (dropCounter > speed) {
+            dropCounter = 0
+            ball.updateBall()
+
+            if (board.numBlocks == 0) {
+                callback("newLevel")
+            }
+
+            if (!ball.move(paddle, board)) { //Bola al agujero
+                ball.removePrevious()
+                lostBall.play()
+                await new Promise(r => setTimeout(r, 2000))
+                callback("newBall")
+            }
+        }
+        paddle.updatePaddle()
+        window.requestAnimationFrame(update)
+    }
+}
+
+game = new Game()
+game.play()
+
+
---
+++ Tetris/main_v2.js
@@ -0,0 +1,285 @@
+
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+const $score = document.querySelector('span')
+const $button = document.querySelector('button')
+
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+const PIECES = [
+    {
+        shape: [
+            [1, 1, 1, 1]
+        ],
+        color: 'grey'
+    },
+    {
+        shape:
+            [
+                [1, 0, 0],
+                [1, 1, 1]
+            ],
+        color: 'lightblue'
+    },
+    {
+        shape: [
+            [0, 0, 1],
+            [1, 1, 1]
+        ],
+        color: 'lightgreen'
+    },
+    {
+        shape: [
+            [0, 0, 1],
+            [1, 1, 1]
+        ],
+        color: 'orange'
+    },
+    {
+        shape:
+            [
+                [1, 1],
+                [1, 1]
+            ],
+        color: 'orange'
+    },
+    {
+        shape:
+            [
+                [0, 1, 1],
+                [1, 1, 0]
+            ],
+        color: 'blue'
+    },
+    {
+        shape:
+            [
+                [0, 1, 0],
+                [1, 1, 1]
+            ],
+        color: 'yellow'
+    },
+    {
+        shape:
+            [
+                [1, 1, 0],
+                [0, 1, 1]
+            ],
+        color: 'lime'
+    }
+]
+
+let dropCounter = 0
+let lastTime = 0
+let gamePaused = true
+
+let speed
+let score
+let piece
+let board
+
+context.fillStyle = '#000'
+context.fillRect(0, 0, canvas.width, canvas.height)
+
+function startGame() {
+    speed = 800
+    score = 0
+    piece = getRandomPiece()
+    board = createBoard()
+    $score.innerText = score
+    draw()
+    gamePaused = false
+    update()
+}
+
+function gameOver() {
+    window.alert("Game Over !!!")
+    gamePaused = true
+    update()
+    $button.innerText = "Nuevo Juego"
+}
+
+function createBoard() {
+    ret = []
+    for (y = 0; y < BOARD_HEIGHT; y++) {
+        row = []
+        for (x = 0; x < BOARD_WIDTH; x++) {
+            row[x] = new Object({ value: 0, color: '' })
+        }
+        ret.push(row)
+    }
+    return ret
+}
+
+function getRandomPiece() {
+    ret = { shape: [], color: '', position: { x: 0, y: 0 } }
+    var index = Math.floor(Math.random() * PIECES.length)
+    ret.shape = PIECES[index].shape
+    ret.color = PIECES[index].color
+    ret.position.x = Math.floor(Math.random() * (BOARD_WIDTH - ret.shape[0].length))
+    ret.position.y = 0
+    score += 5
+    return ret
+}
+
+function update(time = 0) {
+    if (gamePaused) return
+    const deltaTime = time - lastTime
+    lastTime = time
+
+    dropCounter += deltaTime
+
+    if (dropCounter > speed) {
+        piece.position.y++
+
+        if (checkCollision()) {
+            piece.position.y--
+            solidifyPiece()
+            removeRows()
+        }
+        dropCounter = 0
+    }
+
+    draw()
+    window.requestAnimationFrame(update)
+}
+
+function draw() {
+    context.fillStyle = '#000'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    board.forEach((row, y) => {
+        row.forEach((cell, x) => {
+            if (cell.value === 1) {
+                context.fillStyle = cell.color
+                context.fillRect(x, y, 1, 1)
+                context.lineWidth = 0.1
+                context.strokeStyle = "MediumPurple";
+                context.strokeRect(x, y, 1, 1)
+            }
+        })
+    })
+
+    piece.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1) {
+                context.fillStyle = piece.color
+                context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+                context.lineWidth = 0.1
+                context.strokeStyle = "MediumPurple";
+                context.strokeRect(x + piece.position.x, y + piece.position.y, 1, 1);
+            }
+        })
+    })
+}
+
+
+function checkCollision() {
+    return piece.shape.find((row, y) => {
+        return row.find((val, x) => {
+            if (val == 0) return false
+            else if (y + piece.position.y >= BOARD_HEIGHT || (x + piece.position.x >= BOARD_WIDTH) || (x + piece.position.x < 0)) return true
+            else if (board[y + piece.position.y][x + piece.position.x].value != 0) return true
+            else return false
+        })
+    })
+}
+
+
+function solidifyPiece() {
+    piece.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value == 1) {
+                board[y + piece.position.y][x + piece.position.x].value = 1
+                board[y + piece.position.y][x + piece.position.x].color = piece.color
+            }
+        })
+    })
+
+    //Get random new piece
+    piece = getRandomPiece()
+
+    if (checkCollision()) gameOver()
+}
+
+function removeRows() {
+    const rowsToRemove = []
+
+    board.forEach((row, y) => {
+        if (row.every(cell => cell.value == 1)) {
+            rowsToRemove.push(y)
+        }
+    })
+
+    score += 100 * (rowsToRemove.length ** 2)
+    $score.innerText = score
+    speed -= (rowsToRemove.length > 0 ? 50  : 0)
+
+    rowsToRemove.forEach(y => {
+        board.splice(y, 1)
+        newRow = []
+        for (x = 0; x < BOARD_WIDTH; x++) {
+            newRow[x] = new Object({ value: 0, color: '' })
+        }
+        board.unshift(newRow)
+    })
+}
+
+$button.addEventListener('click', function () {
+    if ($button.innerText === "Continuar") {  // Continuamos una partida ya comenzada
+        $button.innerText = "Pausar"
+        gamePaused = false;
+        update()
+    } else if ($button.innerText === "Nuevo Juego") { //Comenzamos una partida desde cero
+        $button.innerText = "Pausar"
+        startGame()
+    } else if ($button.innerText === "Pausar") {//Pausamos una partida en juego
+        $button.innerText = "Continuar"
+        gamePaused = true
+        update()
+    }
+}, false)
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowLeft') {
+        piece.position.x--
+        if (checkCollision()) piece.position.x++
+    }
+    if (event.key === 'ArrowRight') {
+        piece.position.x++
+        if (checkCollision()) piece.position.x--
+    }
+    if (event.key === 'ArrowDown') {
+        piece.position.y++
+        if (checkCollision()) {
+            piece.position.y--
+            solidifyPiece()
+            removeRows()
+        }
+    }
+
+    if (event.key === 'ArrowUp') {
+
+        const rotated = []
+
+        for (i = 0; i < piece.shape[0].length; i++) {
+            const row = []
+            for (j = piece.shape.length - 1; j >= 0; j--) {
+                row.push(piece.shape[j][i])
+            }
+            rotated.push(row)
+        }
+        const previousShape = piece.shape
+        piece.shape = rotated
+        if (checkCollision()) {
+            piece.shape = previousShape
+        }
+    }
+})
