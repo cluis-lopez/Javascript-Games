febd2b431ea0833b86de90875391c9cb93f5b46c
Clopez <cluis.lopez@gmail.com>
2025-06-24 18:49:40 +0200

    Advancing methods

--- HeptaCreate/aux.js
+++ HeptaCreate/aux.js
@@ -74,3 +74,18 @@ function totalElementos(array){
     }
     return result
 }
+
+function weighted_random(items, weights) {
+    var i;
+
+    for (i = 1; i < weights.length; i++)
+        weights[i] += weights[i - 1];
+    
+    var random = Math.random() * weights[weights.length - 1];
+    
+    for (i = 0; i < weights.length; i++)
+        if (weights[i] > random)
+            break;
+    
+    return items[i];
+}
